70f6d55a9e4201791347cb4f142c2b32557ef3b1
Estevaoferreira <estevaoeferreira9984@gmail.com>
2021-06-08 15:23:24 -0300

    teste-PHP

--- conexao.php
+++
@@ -1,3 +0,0 @@
-<?php
-  $conexao = mysqli_connect('localhost','root','','stock_box') or die ('Erro na conexão com localhost'); 
- ?>
--- venda_concluida.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(session_id() == '' || !isset($_SESSION)){session_start();}
-if(!isset($_SESSION["usuario"])) {header("location:index.php");}
-include 'conexao.php';
-date_default_timezone_set('America/Sao_paulo');
-
-$conexao->close();
-
-?>
